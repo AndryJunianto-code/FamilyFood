9305f1ef83d6c911aa49a81f386666ae0a523cfa
AndryJunianto1023 <69632552+AndryJunianto1023@users.noreply.github.com>
2021-06-30 21:49:47 +0800

    l

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,22 @@ import {useState, useEffect} from 'react'
 function App() {
   const [foods, setFoods] = useState([])
 
-  const getFood = async () => {
+ /*  const getFood = async () => {
     const res = await axios.get('/foods')
     setFoods(res.data)
   }
   useEffect(()=> {  
     getFood()
-  },[])
+  },[]) */
   return (
     <div>
-      {foods.map(food=> (
+     {/*  {foods.map(food=> (
         <div key={food._id}>
           <h1 className='text-red-500 text-6xl font-extrabold'>{food.name}</h1>
           <h1 className='text-sm text-blue-500 font-extrabold'>{food.age}</h1>
           </div>
-      ))}
-     
+      ))} */}
+     holaaaa
     </div>
   );
 }
